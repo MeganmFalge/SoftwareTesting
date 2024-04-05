a1ea20fe6fe42dad6e44362fbb28cbb0e83a1e0f
Rachel <rgonzalez2229@eagle.fgcu.edu>
2024-04-05 17:24:22 -0400

    Testing login page. I know it's wrong format but problem for later

--- src/test/java/com/example/demo/MainPageTest.java
+++ src/test/java/com/example/demo/MainPageTest.java
@@ -1,58 +1,81 @@
 package com.example.demo;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import org.openqa.selenium.chrome.ChromeOptions;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.testng.annotations.*;
-
-import static org.testng.Assert.*;
-
-import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 public class MainPageTest {
     MainPage mainPage = new MainPage();
+    WebDriver driver;
 
     @BeforeClass
-    public static void setUpAll() {
-        Configuration.browserSize = "1280x800";
-        SelenideLogger.addListener("allure", new AllureSelenide());
-    }
-
-    @BeforeMethod
-    public void setUp() {
-        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
-        Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
-        open("https://www.jetbrains.com/");
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver",
+                "C:/Users/rache/Documents/testing/chromedriver-win64/chromedriver-win64/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
     @Test
-    public void search() {
-        mainPage.searchButton.click();
+    public void testSuccessfulLogin() {
 
-        $("[data-test='search-input']").sendKeys("Selenium");
-        $("button[data-test='full-search-button']").click();
+        driver.get("http://localhost:4200/");
 
-        $("input[data-test='search-input']").shouldHave(attribute("value", "Selenium"));
+        try {
+            Thread.sleep(2000); // 20 seconds delay
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Enter valid credentials and submit login form
+        driver.findElement(By.name("email")).sendKeys("marcbanghart@gmail.com");
+        driver.findElement(By.name("password")).sendKeys("marcbanghart@gmail.com");
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        // Verify successful login by checking if the user is redirected to a new page
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertNotEquals(currentUrl, "http://localhost:4200/dashboard", "Successful login should redirect user to a new page");
     }
 
     @Test
-    public void toolsMenu() {
-        mainPage.toolsMenu.click();
+    public void testUnsuccessfulLogin() {
+        driver.get("http://localhost:4200/");
 
-        $("div[data-test='main-submenu']").shouldBe(visible);
+        try {
+            Thread.sleep(2000); // 20 seconds delay
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Enter invalid credentials and submit login form
+        driver.findElement(By.name("email")).sendKeys("invalidemail@example.com");
+        driver.findElement(By.name("password")).sendKeys("invalidpassword");
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        try {
+            Thread.sleep(2000); // 20 seconds delay
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Verify unsuccessful login by checking for error messages or remaining on the same page
+        String actualURL = driver.getCurrentUrl();
+        try {
+            Thread.sleep(2000); // 20 seconds delay
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(actualURL, "http://localhost:4200/", "Unsuccessful login should stay on the same page");
     }
 
-    @Test
-    public void navigationToAllTools() {
-        mainPage.seeDeveloperToolsButton.click();
-        mainPage.findYourToolsButton.click();
 
-        $("#products-page").shouldBe(visible);
-
-        assertEquals(Selenide.title(), "All Developer Tools and Products by JetBrains");
+    @AfterTest
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
